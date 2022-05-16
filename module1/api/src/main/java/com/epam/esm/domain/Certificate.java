09c0beb473264de7ca57c78a723c4c67b05a2035
Dzmitry Akulich <dzmitry.akulich.33@gmail.com>
2022-05-16 17:19:28 +0300

    feat: add lombok builder annotation

--- module1/api/src/main/java/com/epam/esm/domain/Certificate.java
+++ module1/api/src/main/java/com/epam/esm/domain/Certificate.java
@@ -2,6 +2,7 @@ package com.epam.esm.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,94 +12,17 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Certificate {
     private Long id;
     private String name;
     private String description;
     private Double price;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime creationDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastUpdateDate;
     private Integer state;
     private Integer duration;
     private List<Tag> tags;
-
-    public static CertificateBuilder builder() {
-        return new CertificateBuilder();
-    }
-
-    public static class CertificateBuilder {
-        private Long id;
-        private String name;
-        private String description;
-        private Double price;
-
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime creationDate;
-
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime lastUpdateDate;
-
-        private Integer state;
-        private Integer duration;
-        private List<Tag> tags;
-
-        public CertificateBuilder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public CertificateBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public CertificateBuilder description(String description) {
-            this.description = description;
-            return this;
-        }
-
-        public CertificateBuilder price(Double price) {
-            this.price = price;
-            return this;
-        }
-
-        public CertificateBuilder creationDate(LocalDateTime creationDate) {
-            this.creationDate = creationDate;
-            return this;
-        }
-
-        public CertificateBuilder lastUpdateDate(LocalDateTime lastUpdateDate) {
-            this.lastUpdateDate = lastUpdateDate;
-            return this;
-        }
-
-        public CertificateBuilder state(Integer state) {
-            this.state = state;
-            return this;
-        }
-
-        public CertificateBuilder duration(Integer duration) {
-            this.duration = duration;
-            return this;
-        }
-
-        public CertificateBuilder tags(final List<Tag> tags) {
-            this.tags = tags;
-            return this;
-        }
-
-        public Certificate build() {
-            return new Certificate(
-                    this.id,
-                    this.name,
-                    this.description,
-                    this.price,
-                    this.creationDate,
-                    this.lastUpdateDate,
-                    this.state,
-                    this.duration,
-                    this.tags
-            );
-        }
-    }
 }
--- module1/api/src/main/java/com/epam/esm/domain/Tag.java
+++ module1/api/src/main/java/com/epam/esm/domain/Tag.java
@@ -1,43 +1,16 @@
 package com.epam.esm.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Tag {
     private Long id;
     private String name;
     private Integer state;
-
-    public static TagBuilder builder() {
-        return new TagBuilder();
-    }
-
-    public static class TagBuilder {
-        private Long id;
-        private String name;
-        private Integer state;
-
-        public TagBuilder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public TagBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public TagBuilder state(Integer state) {
-            this.state = state;
-            return this;
-        }
-
-        public Tag build() {
-            return new Tag(this.id, this.name, this.state);
-        }
-    }
 }
